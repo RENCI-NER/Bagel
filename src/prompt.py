45b6b6f57f455e15c8376f28ae6a22e8b90d1686
YaphetKG <kebedey@renci.org>
2025-06-17 16:35:44 -0400

    prompt backend validation

--- src/prompt.py
+++ src/prompt.py
@@ -5,6 +5,8 @@ from config import PromptSettings, List
 
 def load_prompt_from_hub(prompt_name):
     """ Loads prompts from langchain hub and returns a prompt object."""
+    if  not prompt_name.startswith("bagel/"):
+        raise ValueError("Invalid prompt {prompt_name}. Prompts are supported only from bagel/ namespace currently.".format(prompt_name=prompt_name))
     return hub.pull(str(prompt_name))
 
 
